dd0e5b951371477c7a73e5ac83cea3418739c257
Jimena <jimenanrp2@gmail.com>
2023-01-31 20:05:32 +0100

    finished array

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { Character } from './constructor/Character';
+import { Advisor } from './advisor-class/Advisor';
+import { Fighter } from './fighter-class/Fighter';
+import { King } from './king-class/King';
+import { Squire } from './squire-class/Squire';
--- src/models/Characters/characters.ts
+++ src/models/Characters/characters.ts
@@ -1,1 +1,13 @@
-import {  } from "module";
+import { Advisor } from '../advisor-class/Advisor';
+import { Fighter } from '../fighter-class/Fighter';
+import { King } from '../king-class/King';
+import { Squire } from '../squire-class/Squire';
+
+const joffrey = new King('Joffrey', 'Baratheon', 12, 3);
+const jamie = new Fighter('Jamie', 'Lannister', 35, 'sword', 7);
+const daenerys = new Fighter('Daenerys', 'Targaryen', 18, 'dragon', 6);
+const tyrion = new Advisor('Tyrion', 'Lannister', 40, daenerys);
+const bronn = new Squire('Bronn', 'Darkwaters', 32, 1, jamie);
+
+
+const characters = [joffrey, jamie, daenerys, tyrion, bronn];
