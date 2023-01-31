bb32482233565f7139ee998a6b9740f2ff835225
Jimena <jimenanrp2@gmail.com>
2023-01-31 18:29:39 +0100

    📝 Organize ts classes

---
+++ src/squire-class/Squire-class.ts
@@ -0,0 +1,18 @@
+import {Character} from '../constructor/Character';
+import {type Fighter} from '../fighter-class/Fighter-class';
+
+export class Squire extends Character {
+	constructor(
+		name: string,
+		family: string,
+		age: number,
+		public grade: number,
+		public boss: Fighter,
+	) {
+		super(name, family, age);
+	}
+
+	greetings(): string {
+		return 'Soy un loser';
+	}
+}
